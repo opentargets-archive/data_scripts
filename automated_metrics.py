870e2b76b794784f5ba5228bbcd9a84457ac5cd3
Asier Gonzalez <gonzaleza@ebi.ac.uk>
2020-04-09 17:05:24 +0100

    Refactored automated_metrics.py

--- automated_metrics.py
+++ automated_metrics.py
@@ -2,6 +2,7 @@
 import json
 import pandas as pd
 from elasticsearch import Elasticsearch
+import argparse
 
 __copyright__ = "Copyright 2014-2019, Open Targets"
 __author__ = "Michaela Spitzer, Asier Gonzalez"
@@ -12,27 +13,31 @@ __maintainer__= "Asier Gonzalez"
 __email__     = ["data@opentargets.org"]
 __status__    = "Prototype"
 
-def connect_elasticsearch():
-    _es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+def connect_elasticsearch(host, port):
+    _es = Elasticsearch([{'host': host, 'port': port}])
     if _es.ping():
         print('Yay Connected')
     else:
         print('Awww it could not connect!')
     return _es
 
-# Connect to ES and run the queries
-es = connect_elasticsearch()
-if es is not None:
-    # Get nr of valid evidence strings
+def build_nr_evidence_query():
+    """ Build query to get number of valid evidence strings """
     query_nr_evidence_strings = {"query": { "match_all": {} }, "aggs": {"evidence_string_numbers": {"terms": {"field": "private.datasource.keyword","size": 20 }}}, "size": 0}
-    valid_evidence = es.search('20.02_evidence-data', json.dumps(query_nr_evidence_strings))
-    # Get number of invalid evidence strings
+    return query_nr_evidence_strings
+
+def build_nr_invalid_evidence_query():
+    """ Build query to get number of invalid evidence strings """
     query_nr_inv_evidence_strings = {"query": { "match_all": {} }, "aggs": {"evidence_string_numbers": {"terms": {"field": "data_source.keyword","size": 20 }}}, "size": 0}
-    invalid_evidence = es.search('20.02_invalid-evidence-data', json.dumps(query_nr_inv_evidence_strings))
-    # Get nr of evidence strings with score == 0
+    return query_nr_inv_evidence_strings
+
+def build_nr_evidence_score0_query():
+    """ Build query to get number of evidence strings with score == 0 """
     query_nr_evidence_strings_score0 = {"query": {"match": {"scores.association_score": 0}},"aggs": {"evidence_string_numbers": {"terms": {"field": "private.datasource.keyword","size": 20}}}, "size": 0}
-    score0_evidence = es.search('20.02_evidence-data', json.dumps(query_nr_evidence_strings_score0))
-    # Get nr of associations data sources contribute to
+    return query_nr_evidence_strings_score0
+
+def build_nr_assocs_per_datasource_query():
+    """ Build query to get number of associations each data sources contribute to """
     query_associations = {"query": {"match_all": {}},"size": 0,
                           "aggs": {
                               "counts": {
@@ -56,65 +61,109 @@ if es is not None:
                                                        "uniq_targets": {"cardinality": {"field": "target.gene_info.geneid.keyword"}},
                                                        "uniq_diseases": {"cardinality": {"field": "disease.efo_info.efo_id.keyword"}}}}}}
 
-    counts_associations = es.search('20.02_association-data', json.dumps(query_associations), request_timeout=30)
+    return query_associations
 
-# Create a dictionary for all 20 datasources
-datasources = {'cancer_gene_census':{},
-               'chembl':{},
-               'crispr':{},
-               'europepmc':{},
-               'eva':{},
-               'eva_somatic':{},
-               'expression_atlas':{},
-               'gene2phenotype':{},
-               'genomics_england':{},
-               'ot_genetics_portal':{},
-               'intogen':{},
-               'phenodigm':{},
-               'phewas_catalog':{},
-               'progeny':{},
-               'reactome':{},
-               'slapenrich':{},
-               'sysbio':{},
-               'uniprot':{},
-               'uniprot_literature':{},
-               'uniprot_somatic':{}}
+def es_search(es,prefix, index, query):
+    """ Query elasticsearch search endpoint """
+    return es.search(prefix + index, json.dumps(query), request_timeout=30)
 
-# Create fields for all 8 parameters of interest
-for key in datasources:
-    datasources[key]={
-        'valid_evidence_strings': 0,
-        'invalid_evidence_strings': 0,
-        'score_is_0': 0,
-        'associations_direct': 0,
-        'diseases_direct': 0,
-        'targets_direct': 0,
-        'associations_all': 0,
-        'diseases_all': 0
-    }
-# Retrieve the numbers from each of the 4 ES queries
-for item in valid_evidence['aggregations']['evidence_string_numbers']['buckets']:
-    datasources[item['key']]['valid_evidence_strings'] = item['doc_count']
-for item in invalid_evidence['aggregations']['evidence_string_numbers']['buckets']:
-    datasources[item['key']]['invalid_evidence_strings'] = item['doc_count']
-for item in score0_evidence['aggregations']['evidence_string_numbers']['buckets']:
-    datasources[item['key']]['score_is_0'] = item['doc_count']
+def process_es_response(valid_evidence, invalid_evidence, score0_evidence, counts_associations, outfile):
+    # Create a dictionary for all 20 datasources
+    datasources = {'cancer_gene_census':{},
+                   'chembl':{},
+                   'crispr':{},
+                   'europepmc':{},
+                   'eva':{},
+                   'eva_somatic':{},
+                   'expression_atlas':{},
+                   'gene2phenotype':{},
+                   'genomics_england':{},
+                   'ot_genetics_portal':{},
+                   'intogen':{},
+                   'phenodigm':{},
+                   'phewas_catalog':{},
+                   'progeny':{},
+                   'reactome':{},
+                   'slapenrich':{},
+                   'sysbio':{},
+                   'uniprot':{},
+                   'uniprot_literature':{},
+                   'uniprot_somatic':{}}
 
-for item in counts_associations['aggregations']['counts_direct']['counts']['buckets']:
-    datasources[item['key']]['associations_direct'] = item['doc_count']
-    datasources[item['key']]['diseases_direct'] = item['uniq_diseases']['value']
-    datasources[item['key']]['targets_direct'] = item['uniq_targets']['value']
+    # Create fields for all 8 parameters of interest
+    for key in datasources:
+        datasources[key]={
+            'valid_evidence_strings': 0,
+            'invalid_evidence_strings': 0,
+            'score_is_0': 0,
+            'associations_direct': 0,
+            'diseases_direct': 0,
+            'targets_direct': 0,
+            'associations_all': 0,
+            'diseases_all': 0
+        }
+    # Retrieve the numbers from each of the 4 ES queries
+    for item in valid_evidence['aggregations']['evidence_string_numbers']['buckets']:
+        datasources[item['key']]['valid_evidence_strings'] = item['doc_count']
+    for item in invalid_evidence['aggregations']['evidence_string_numbers']['buckets']:
+        datasources[item['key']]['invalid_evidence_strings'] = item['doc_count']
+    for item in score0_evidence['aggregations']['evidence_string_numbers']['buckets']:
+        datasources[item['key']]['score_is_0'] = item['doc_count']
 
-for item in counts_associations['aggregations']['counts']['buckets']:
-    datasources[item['key']]['associations_all'] = item['doc_count']
-    datasources[item['key']]['diseases_all'] = item['uniq_diseases']['value']
+    for item in counts_associations['aggregations']['counts_direct']['counts']['buckets']:
+        datasources[item['key']]['associations_direct'] = item['doc_count']
+        datasources[item['key']]['diseases_direct'] = item['uniq_diseases']['value']
+        datasources[item['key']]['targets_direct'] = item['uniq_targets']['value']
 
-# Collect the data in a panda dataframe to write into a csv file
-df = pd.DataFrame(columns=datasources['eva'].keys())
-myrow=0
-for key in datasources:
-    df.loc[myrow] = list(datasources[key].values())
-    myrow=myrow+1
+    for item in counts_associations['aggregations']['counts']['buckets']:
+        datasources[item['key']]['associations_all'] = item['doc_count']
+        datasources[item['key']]['diseases_all'] = item['uniq_diseases']['value']
 
-df.insert(0, "datasource", datasources.keys(), True)
-df.to_csv("ot_metrics.csv", index=False)
+    # Collect the data in a panda dataframe to write into a csv file
+    df = pd.DataFrame(columns=datasources['eva'].keys())
+    myrow=0
+    for key in datasources:
+        df.loc[myrow] = list(datasources[key].values())
+        myrow=myrow+1
+
+    df.insert(0, "datasource", datasources.keys(), True)
+    df.to_csv(outfile, index=False)
+
+def main():
+
+    # Parse CLI parameters
+    parser = argparse.ArgumentParser(description='Script that extracts metrics from elasticsearch server.')
+    parser.add_argument('--esPrefix', help='Prefix of ES indices, e.g. 20.04', type=str, required=True)
+    parser.add_argument('-o','--outputFile', help='Name of the output file', type=str, default='ot_metrics.csv')
+    parser.add_argument('--host', help='Name or IP address of the ES server, "localhost" by default', type=str, default='localhost')
+    parser.add_argument('--port', help='Port number where ES is listening, 9200 by default', type=int, default=9200)
+    args = parser.parse_args()
+
+    # Parse input parameters
+    es_prefix = args.esPrefix
+    outfile = args.outputFile
+    host = args.host
+    port = args.port
+
+    # Connect to ES
+    es = connect_elasticsearch(host, port)
+
+    if es is not None:
+        query_nr_evidence_strings = build_nr_evidence_query()
+        valid_evidence = es_search(es, es_prefix, "_evidence-data", query_nr_evidence_strings)
+
+        query_nr_inv_evidence_strings = build_nr_invalid_evidence_query()
+        invalid_evidence = es_search(es, es_prefix, "_invalid-evidence-data", query_nr_inv_evidence_strings)
+
+        query_nr_evidence_strings_score0 = build_nr_evidence_score0_query()
+        score0_evidence = es_search(es, es_prefix, "_evidence-data", query_nr_evidence_strings_score0)
+
+        query_associations_per_datasource = build_nr_assocs_per_datasource_query()
+        counts_associations = es_search(es, es_prefix, "_association-data", query_associations_per_datasource)
+
+        process_es_response(valid_evidence, invalid_evidence, score0_evidence, counts_associations, outfile)
+
+
+
+if __name__ == "__main__":
+    main()
